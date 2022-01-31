80e56b590104a1a1a261dbc91f7133520c9c7234
salmanyulio <muhammadyulio30@smk.belajar.id>
2022-01-31 15:19:06 +0700

    Menambahkan migrations

---
+++ database/migrations/2022_01_18_023748_create_detail_transaksi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_transaksi')->constrained('transaksi');
+            $table->foreignId('id_paket')->constrained('paket');
+            $table->double('qty');
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transaksi');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Guzzle\Http\Promise\Create;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(3)->create();
+
+        User::create([
+            'name' => 'Admin',
+            'username' => 'Admin01',
+            'email' => 'admin1@gmail.com',
+            'password' => bcrypt('12345678'),
+            'id_outlet' => '1',
+            'role' => 'admin'
+        ]);
     }
 }
