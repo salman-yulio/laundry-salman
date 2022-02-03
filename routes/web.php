d73f683de3c1ca55e0f5691d61dbc15f607de3cd
salmanyulio <muhammadyulio30@smk.belajar.id>
2022-02-03 13:14:23 +0800

    Menambahkan Fitur Login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PaketController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\OutletController;
@@ -17,9 +18,21 @@ use App\Http\Controllers\OutletController;
 */
 
 Route::get('/', function () {
-    return view('dashboard.index');
+    return view('login.index');
 });
 
-Route::resource('/dashboard/member', MemberController::class);
-Route::resource('/dashboard/outlet', OutletController::class);
-Route::resource('/dashboard/paket', PaketController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::get('/dashboard/user', function () {
+    return view('dashboard.user.index');
+})->middleware('auth');
+
+Route::get('/', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::resource('/dashboard/member', MemberController::class)->middleware('auth');
+Route::resource('/dashboard/outlet', OutletController::class)->middleware('auth');
+Route::resource('/dashboard/paket', PaketController::class)->middleware('auth');
